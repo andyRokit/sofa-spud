27ef503a09d3d62dcbee474ba62222d325459c14
Andrew <andrew@complybots.com>
2022-07-26 11:31:57 +0100

    Initial commit

---
+++ test/sofa-spud.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SofaSpud from '../lib/sofa-spud-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sofa-spud-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SofaSpud.SofaSpudStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
